174e03e0cab414406bbbfcb6c46ddfc0ebe92108
Aleksander Jess <jess.aleksander@gmail.com>
2020-04-24 16:32:17 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using static System.Console;
+
+namespace WorkingWithNetwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isValid = false;
+            Write("Enter a valid web address and press enter: ");
+            string url = ReadLine();
+			if(string.IsNullOrWhiteSpace(url))
+			{
+                while (isValid == false)
+				{
+                    WriteLine("Try again");
+                    Write("Enter a valid web address and press enter: ");
+                    url = ReadLine();
+					if(string.IsNullOrWhiteSpace(url))
+					{
+                        continue;
+                    } 
+					else {
+                        isValid = true;
+                        break;
+                    }
+                }
+            }
+            Uri uri = new Uri(url);
+			IPHostEntry entry = Dns.GetHostEntry(uri.Host);
+
+            WriteLine($"URL: {url}");
+            WriteLine($"Scheme: {uri.Scheme}");
+            WriteLine($"Port: {uri.Port}");
+            WriteLine($"Host: {uri.Host}");
+            WriteLine($"Path: {uri.AbsolutePath}");
+            WriteLine("======");
+            WriteLine("Some basic info about the DNS entry for your page:");
+            WriteLine($"{url}'s host name is {entry.HostName}");
+            WriteLine($"{entry.HostName} has the following IP addresses:");
+			foreach (IPAddress address in entry.AddressList)
+			{
+                WriteLine($"{address}");
+            }
+    		
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# WorkingWithNetwork
+
+A basic CLI app that gives you some basic info about a url you input.
+
+Built in .NET Core 3.1. 
